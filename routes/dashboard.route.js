7638dd0ea6fdf362b6930fc601a9dfbb1fd9b8a5
notshanxx <manuelmarkvince@gmail.com>
2024-09-02 20:45:30 +0800

    added /admin route

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createQuote } from "../controllers/api.controller.js";
 import { authenticate } from "../middleware/authenticateUser.js";
 import { getUserInfo } from "../middleware/getUserInfo.js";
+import quoteModel from "../models/quote.model.js";
 const router = express.Router()
 
 
@@ -11,5 +12,23 @@ router.get('/dashboard' , authenticate, getUserInfo, (req, res) =>{
 })
 ,
 router.post('/dashboard', authenticate, getUserInfo, createQuote)
+
+router.get("/admin", async(req, res) => {
+    const allQuotes = await quoteModel.find()
+    const quotesApproved = allQuotes.filter((quote) => quote.info.approved);
+    const quotesNotApproved = allQuotes.filter((quote) => !quote.info.approved);
+    res.render("admin-dashboard",{quotes:allQuotes, quotesApproved, quotesNotApproved});
+  });
+// router.post("/admin", async(req, res) => {
+//     const { id } = req.body;
+//     const quote = await quoteModel.findById(id);
+//     quote.info.approved = true;
+//     await quote.save();
+//     res.redirect("/admin");
+//   });
+
+  
+
+
 
 export default router
